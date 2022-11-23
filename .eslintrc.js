7bb6853eb38ac933fdccca89c8d3783e2bda56ac
David Rodriguez Garcia <davidnichero@gmail.com>
2022-11-23 23:29:44 +0100

    imrpoved eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     'eslint-config-prettier',
     'universe',
     'universe/shared/typescript-analysis',
+    'plugin:react/jsx-runtime',
   ],
+  rules: {
+    'react-native/no-inline-styles': 'off',
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx', '*.d.ts'],
