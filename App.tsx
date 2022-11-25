9ae679dd436b2f93fc4b3f2a65c3817aedd289d5
David Rodriguez Garcia <davidnichero@gmail.com>
2022-11-25 20:10:39 +0100

    added react-navigation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
 import { Feed } from './src/ui/Feed'
 
+const Stack = createNativeStackNavigator()
+
 export default function App() {
-  return <Feed />
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name={'Feed'} component={Feed} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
