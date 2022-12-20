6e10134d55dbae46ddbc1761eb14a1ac532d459d
David Rodriguez Garcia <davidnichero@gmail.com>
2022-12-20 10:30:51 +0100

    added missing mock to App.ispec.tsx

--- src/ui/_navigation/__tests__/App.ispec.tsx
+++ src/ui/_navigation/__tests__/App.ispec.tsx
@@ -1,11 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { fireEvent, render, waitForElementToBeRemoved } from '@testing-library/react-native'
+import { asFunction } from 'awilix'
 
+import { container } from '../../../core/_di'
+import { aSetOfArticles } from '../../../core/domain/model/Article/Article.model'
 import AppNavigator from '../AppNavigator'
 
 jest.useFakeTimers()
 
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
+
+container.register({
+  getArticles: asFunction(() => aSetOfArticles()),
+})
 
 describe('App', () => {
   it('navigates to the Detail screen on artile press', () => {
